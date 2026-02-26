17b215841855c0d2f79d3ce1ad07d15ba215665e
sala7khaled <sala7khalad@gmail.com>
2026-02-26 17:12:08 +0200

    Initial Commit

---
+++ MorphTabBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MorphTabBar
+//
+//  Created by Salah Khaled on 26/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MorphTabBar/MorphTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphTabBarApp.swift
+//  MorphTabBar
+//
+//  Created by Salah Khaled on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MorphTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
